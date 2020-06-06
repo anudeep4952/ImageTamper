5af038fae7e9fead50cd2b0af37b3ecdcd5fdd6d
bazigar <anudeep.insvirat@gmail.com>
2020-06-06 16:02:58 +0530

    tamper detection1

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from detection import detection
 from susbtitute import substitute
 from tamper import tamper
+import sys
+print('Python %s on %s' % (sys.version, sys.platform))
+sys.path.extend(['/media/bazigar/DATA/anudeep/Desktop/ImageTamperDetection'])
 
 while True:
     ch=int(input("1.WaterMark Image 2.Tamper Detection 3.Tamper 4.Exit"))
